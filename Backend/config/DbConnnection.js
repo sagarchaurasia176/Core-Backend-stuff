4524076fa4457f2068e5b8a164bfd862bcf2af74
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-07-30 11:49:40 +0530

    db problem solved !

--- Backend/config/DbConnnection.js
+++ Backend/config/DbConnnection.js
@@ -11,13 +11,12 @@ const dbConnection = async () => {
     await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-     
     });
     console.log("Database connected successfully!");
   } catch (error) {
     console.error("Database connection failed!");
     console.error(`Error: ${error.message}`);
-  
+
     process.exit(1); // Exit the process with failure
   }
 };
