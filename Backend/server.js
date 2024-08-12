2664db5823f2f80e01f1464e5df66e1e149e140b
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-08-12 16:54:09 +0530

    Integrate with the cloudinary , and send the files to the cloud

--- Backend/server.js
+++ Backend/server.js
@@ -4,16 +4,31 @@ const routes = require("./routes/MainRoutes");
 const LoginRoute = require("./routes/log/LoginRoutes");
 require("dotenv").config();
 const dbConnection = require("./config/DbConnnection");
-//cookies parser
-const cookieParser = require('cookie-parser');
 
+//file upload  in the server
+const fileUpload = require("express-fileupload");
+
+//cookies parser
+const cookieParser = require("cookie-parser");
 // Environment variable configuration
 const port = process.env.PORT || 8000;
+
+//cloudinary for cloud purpose
+const cloudinary = require("./config/Cloudinary");
+const routerUploader = require("./routes/upload/UploadRoutes");
+
 // Middleware for parsing JSON bodies
 app.use(express.json());
+//cookie-parser
+app.use(cookieParser());
 
+//file upload
+app.use(fileUpload());
 // Database connection
 dbConnection();
+
+//cloudinary fuction called
+cloudinary();
 
 // Server status route
 app.get("/", (req, res) => {
@@ -25,6 +40,8 @@ app.use("/api/blog", routes);
 // Authentication routes
 app.use("/api/auth", LoginRoute);
 
+//cloudinary routes
+app.use("/api/upload", routerUploader);
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
