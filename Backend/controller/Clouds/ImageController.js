c0dc5683f16788130c0e9b5f63b32f31df2153b9
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-08-13 18:06:42 +0530

    added the video routes also for stored the videos to the cloud , but some minor issues so I checked after that

--- Backend/controller/Clouds/ImageController.js
+++ Backend/controller/Clouds/ImageController.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const CloudMedia = require("../../model/cloud/MediaSchema");
 //Now we've to start and send the image
 const cloudinary = require("cloudinary").v2;
@@ -5,11 +6,10 @@ require("dotenv").config();
 
 //cloudinary config apply here so we gets
 cloudinary.config({
-  Cloudinary_Api: process.env.Cloudinary_Api,
-  Cloudinary_Secret: process.env.Cloudinary_Secret,
-  Cloudinary_Name: process.env.Cloudinary_Name,
+  cloud_name: process.env.Cloudinary_Name,
+  api_key: process.env.Cloudinary_Api,
+  api_secret: process.env.Cloudinary_Secret,
 });
-//Image Controller apply there so we get
 
 //validation for file format
 const isFileFormat = async (fileType, fileExtensionName) => {
@@ -19,7 +19,12 @@ const isFileFormat = async (fileType, fileExtensionName) => {
 //upload the image to the cloud so we get
 const uploadImage = async (ImageUploadfile, Folder) => {
   const options = { Folder };
-  await cloudinary.uploader.upload(ImageUploadfile.tempPath, options);
+  //here temp => is the server name of the express file upload
+  // await cloudinary.uploader.upload(ImageUploadfile.tempFileDir, options);
+  return await cloudinary.uploader.upload(
+    ImageUploadfile.tempFilePath,
+    options
+  );
 };
 
 //Image Controller here
@@ -30,11 +35,20 @@ exports.ImageController = async (req, res) => {
     console.log(name, tag, email);
 
     //express file-upload method here !
-    const fileChek = req.files.Img;
 
+    const fileChek = req.files.Images;
     //fILeFormat supported
     const fileFormatTypeSupported = ["jpg", "png", "jpeg"];
+
+    //here we delete the .JPG => And then convert it into the lowerscase !
     const fileExtensionName = fileChek.name.split(".")[1].toLowerCase();
+
+    if (!fileChek) {
+      return res.json({
+        success: false,
+        message: "file missed",
+      });
+    }
 
     //validation for file format here so we get,
     if (!isFileFormat(fileFormatTypeSupported, fileExtensionName)) {
@@ -44,18 +58,19 @@ exports.ImageController = async (req, res) => {
         error: er.message,
       });
     }
-    //Now start to upload the image
-    const cloudinaryResponseFromTheServer = await uploadImage(
-      fileChek,
-      "nodemailer"
-    );
-    console.log(cloudinaryResponseFromTheServer + "cloud response server");
 
+    //Now start to upload the image
+    const cloudinaryResponseFromTheServer = await uploadImage(fileChek, "pyp");
+
+    //Cloudinary response to the server
+    console.log(cloudinaryResponseFromTheServer + "cloud response server");
     //create entry into the Db
     const MediaUploadedToTheServer = await CloudMedia.create({
       name,
       tag,
       email,
+      //The data is basically comes from the file and that is media uploaded to the server
+      Url: cloudinaryResponseFromTheServer.secure_url,
     });
 
     // returnn the respone
@@ -69,7 +84,7 @@ exports.ImageController = async (req, res) => {
   } catch (er) {
     return res.status(404).json({
       success: false,
-      message: "File not uploaded to cloud",
+      message: "File not uploaded to cloud might be some errors !",
       error: er.message,
     });
   }
