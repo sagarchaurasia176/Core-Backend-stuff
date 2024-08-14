4d515f2adbe244ec09ef63a4b8e73205a9a0c45f
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-08-14 09:21:12 +0530

    nodemailer setup done

--- Backend/model/cloud/MediaSchema.js
+++ Backend/model/cloud/MediaSchema.js
@@ -18,7 +18,21 @@ const MediaScheam = new mongoose.Schema({
         type:String,
 
     }
-
 });
+
+//Create the post middleware 
+MediaScheam.post('save' , async function mailer(doc) {
+        try{
+            console.log(doc);
+
+        }catch(er){
+            console.log("error",er);
+        }
+
+})
+
+
+
+
 //exports that media schema here
 module.exports  = mongoose.model('CloudMedia' , MediaScheam);
